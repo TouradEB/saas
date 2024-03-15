4f65cd052fc198b304a3c689f09fb4d4ad6db5a9
tourad ebnou <touadebnou0@gmail.com>
2024-03-14 19:02:49 -0700

    Initial commit: Add Next.js project

---
+++ src/app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/providers/theme-provider";
+import ModalProvider from "@/providers/modal-provider";
+import { Toast } from "@/components/ui/toast";
+import { Toaster } from "@/components/ui/toaster";
+
+const dmSans = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Ebnou",
+  description: "All in one Agency Solution",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    
+       <html 
+       lang="en"
+       suppressHydrationWarning
+       >
+        <body className={dmSans.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ModalProvider>
+             {children}
+             <Toaster />
+          </ModalProvider>
+        
+        </ThemeProvider>
+        </body>
+    </html>
+  
+   
+  );
+}
